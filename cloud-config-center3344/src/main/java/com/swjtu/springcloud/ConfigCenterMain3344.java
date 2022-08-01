fabe6431a6b93dee960cea99353ddd381edb6a7b
LilBoat <GuodanYang_nn@163.com>
2022-08-01 22:53:06 +0800

    Config的学习： 利用远端服务器统一进行配置ConfigServer 客户端读取远端的配置 注解：@RefreshScope

---
+++ cloud-config-center3344/src/main/java/com/swjtu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.swjtu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: Lil_boat
+ * @Date: 2022/8/1 21:26
+ * @Description:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+
+}
---
+++ cloud-config-client-3355/src/main/java/com/swjtu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.swjtu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Lil_boat
+ * @Date: 2022/8/1 22:04
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+
+}
