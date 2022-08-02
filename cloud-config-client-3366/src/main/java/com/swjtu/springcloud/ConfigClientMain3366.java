e187ea8dbb4f170d284c90a185939b56c791616e
LilBoat <GuodanYang_nn@163.com>
2022-08-02 13:02:02 +0800

    Bus学习： 两种模式：一：通过一个微服务来通知其他的微服务、二：通过configserver来通知所有的微服务配置已变更。 RabbitMQ

---
+++ cloud-config-client-3366/src/main/java/com/swjtu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.swjtu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Lil_boat
+ * @Date: 2022/8/2 12:33
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+
+}
